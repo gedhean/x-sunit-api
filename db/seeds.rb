98e6502ce9dc81ab1b841579c882c690c5d450bd
gedhean <gedhean.dev@gmail.com>
2018-10-20 18:04:51 -0300

    Create 20 fake survivors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do 
+20.times do 
 	Survivor.create({
 		name: Faker::Name.name,
 		age: Faker::Number.between(2, 100),
