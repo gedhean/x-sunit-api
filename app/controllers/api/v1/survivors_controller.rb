50cf38eb29b4e175bfc62cc3778f21a5101d447f
gedhean <gedhean.dev@gmail.com>
2018-10-21 14:05:55 -0300

    SurvivorController refectored

--- app/controllers/api/v1/survivors_controller.rb
+++ app/controllers/api/v1/survivors_controller.rb
@@ -1,54 +1,56 @@
-module Api::V1
-	class SurvivorsController < ApplicationController
-		# List all Survivors
-		def index
-			survivors = Survivor.order('name');
-			render json: { status: 'success', data: survivors }, status: :ok
+class Api::V1::SurvivorsController < ApplicationController
+	before_action :set_survivor, only: [:show, :update, :destroy]
+
+	# List all Survivors
+	def index
+		@survivors = Survivor.order('name ASC');
+		render json: { success: true, results: @survivors }, status: :ok
+	end
+
+	# List survivor by id
+	def show
+		unless @survivor.nil?
+			render json: { success: true, message: 'Survivor found', result: @survivor }, status: :ok
+		else
+			render json: { success: false, message: 'Survivor not found' }, status: :unprocessable_entity
+		end
+	end
+
+	# Create a new survivor
+	def create
+		@survivor = Survivor.new(survivor_params)
+
+		if @survivor.save
+			render json: { success: true, message: 'Survivor created', result: @survivor }, status: :ok
+		else
+			render json: { success: false, message: 'Failed to create survivor', result: @survivor }, status: :unprocessable_entity
+		end	
+	end
+
+	# Delete an survivor
+	def destroy
+		@survivor.destroy
+		render json: { success: true, message: 'Survivor deleted', result: @survivor }, status: :ok
+
+	end
+
+	# Update an survivor
+	def update
+		if @survivor.update_attributes(survivor_params)
+			render json: { success: true, message: 'Survivor updated', result: @survivor }, status: :ok
+		else
+			render json: { success: false, message: 'Failed to update survivor', result: @survivor }, status: :unprocessable_entity
 		end
 
-		# List survivor by id
-		def show
-			survivor = Survivor.find(params[:id])
-			if survivor
-				render json: { status: 'success', message: 'survivor found', data: survivor }, status: :ok
-			else
-				render json: { status: 'error', message: 'survivor not found' }, status: :unprocessable_entity
-			end
-		end
+	end
 
-		# Create a new survivor
-		def create
-			survivor = Survivor.new(user_params)
+	private
+	def survivor_params
+		params.permit(:name, :age, :gender, :latitude, :longitude)
+	end
 
-			if survivor.save
-				render json: { status: 'success', message: 'survivor created', data: survivor }, status: :ok
-			else
-				render json: { status: 'error', message: 'survivor not created!', data: survivor }, status: :unprocessable_entity
-			end	
-		end
-
-		# Delete an survivor
-		def destroy
-			survivor = Survivor.find(params[:id])
-			survivor.destroy
-			render json: { status: 'success', message: 'survivor deleted', data: survivor }, status: :ok
-
-		end
-
-		# Update an survivor
-		def update
-			survivor = Survivor.find(params[:id])
-			if survivor.update_attributes(user_params)
-				render json: { status: 'success', message: 'survivor updated', data: survivor }, status: :ok
-			else
-				render json: { status: 'error', message: 'survivor not updated', data: survivor }, status: :unprocessable_entity
-			end
-
-		end
-
-		private
-		def user_params
-			params.permit(:name, :age, :gender, :latitude, :longitude)
-		end
+	private
+	def set_survivor
+		@survivor = Survivor.find(params[:id])
 	end
 end
