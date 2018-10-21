ad114b60e3809188adf7e8fa7f2f978d8db1de4a
gedhean <gedhean.dev@gmail.com>
2018-10-21 14:06:52 -0300

    Lifecicle method added to Report model
    Trigger an update to survivors to flag suvivor as abducted

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,13 @@
 class Report < ApplicationRecord
+	validates :reporter_id, :abducted_id, presence: true
+
+	after_save :update_falg_abducted_in_survivors
+
+	private
+	def update_falg_abducted_in_survivors
+		num_reports =  Report.where(abducted_id: abducted_id).count
+		unless num_reports < 3
+			Survivor.update(abducted_id, abducted: true)
+		end 
+	end
 end
