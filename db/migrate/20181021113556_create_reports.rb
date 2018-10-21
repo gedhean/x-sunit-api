356eac41e7b07324cd8d2f74a3050876d5e900e3
gedhean <gedhean.dev@gmail.com>
2018-10-21 12:22:44 -0300

    Removed composite primary key from reports

--- db/migrate/20181021113556_create_reports.rb
+++ db/migrate/20181021113556_create_reports.rb
@@ -1,9 +1,9 @@
 class CreateReports < ActiveRecord::Migration[5.2]
   def change
-    create_table(:reports, primary_key: [:reporter_id, :abducted_id]) do |t|
+    create_table(:reports) do |t|
       t.integer :reporter_id
-      t.text :message
       t.integer :abducted_id
+      t.text :message
 
       t.timestamps
     end
