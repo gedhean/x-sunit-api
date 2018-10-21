26a645b250312b4f6b6dc43587868f0c0c8cd60a
gedhean <gedhean.dev@gmail.com>
2018-10-21 09:15:54 -0300

    Reports Model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_205657) do
+ActiveRecord::Schema.define(version: 2018_10_21_113556) do
+
+  create_table "reports", primary_key: ["reporter_id", "abducted_id"], force: :cascade do |t|
+    t.integer "reporter_id"
+    t.text "message"
+    t.integer "abducted_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "survivors", force: :cascade do |t|
     t.string "name"
