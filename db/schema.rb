f00df831f81625b9a074885c96c211ce9e178744
gedhean <gedhean.dev@gmail.com>
2018-10-21 11:23:40 -0300

    Database schema updated
    Add refs to survivors in table reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_113556) do
+ActiveRecord::Schema.define(version: 2018_10_21_124639) do
 
   create_table "reports", primary_key: ["reporter_id", "abducted_id"], force: :cascade do |t|
     t.integer "reporter_id"
