3414b47d5f28d6add6d53f1daa15259e66c1741b
gedhean <gedhean.dev@gmail.com>
2018-10-21 12:23:40 -0300

    Add double refs to survivors

--- db/migrate/20181021121823_add_ref_to_reports.rb
+++ db/migrate/20181021121823_add_ref_to_reports.rb
@@ -1,5 +1,6 @@
 class AddRefToReports < ActiveRecord::Migration[5.2]
   def change
-  	add_foreign_key :reports, :survivors, column: [:reporter_id, :abducted_id]
+  	add_foreign_key :reports, :survivors, column: :reporter_id
+  	add_foreign_key :reports, :survivors, column: :abducted_id
   end
 end
