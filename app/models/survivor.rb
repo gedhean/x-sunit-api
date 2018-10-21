9c084e3b07aff676638cce26370d6693205ea53c
gedhean <gedhean.dev@gmail.com>
2018-10-21 14:57:05 -0300

    New validations rulas added to Survivor model

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,2 +1,7 @@
 class Survivor < ApplicationRecord
+	validates :name, :age, :gender, :latitude, :longitude, presence: true
+	validates :name, length: { minimum: 4 }
+	validates :age, :latitude, :longitude, numericality: true
+	validates :abducted, inclusion: { in: [true, false] }
+	validates :abducted, exclusion: { in: [nil]}
 end
