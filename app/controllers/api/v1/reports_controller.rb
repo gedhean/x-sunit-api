48ea92834e5457ab714be56f246855e89dd56ceb
gedhean <gedhean.dev@gmail.com>
2018-10-21 14:04:58 -0300

    Add create action to ReportsController
    Def report_param function too

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::ReportsController < ApplicationController
 		@reports = Report.all
 		render json: { results: @reports }, status: :ok		
 	end	
+	# POST api/v1/reports
+	def create
+		@report = Report.new(report_params)
+		if @report.save
+			render json: { success: true, message: "Report created", report: @report }, status: :ok
+		else
+			render json: { success: false, message: "Failed to create report"}, status: :unprocessable_entity
+		end
+	end
+
+	private
+	def report_params
+		params.permit(:reporter_id, :abducted_id, :message)
+	end
 end
