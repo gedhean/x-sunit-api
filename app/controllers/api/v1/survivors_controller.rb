4aa07a5d4c5bce401e0160976590e7910beb1a6c
gedhean <gedhean.dev@gmail.com>
2018-10-22 05:57:15 -0300

    New error handling approach

--- app/controllers/api/v1/survivors_controller.rb
+++ app/controllers/api/v1/survivors_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::SurvivorsController < ApplicationController
 	before_action :set_survivor, only: [:show, :update, :destroy]
+	rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+	rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 	# List all Survivors
 	def index
@@ -9,39 +11,26 @@ class Api::V1::SurvivorsController < ApplicationController
 
 	# List survivor by id
 	def show
-		unless @survivor.nil?
-			render json: { success: true, message: 'Survivor found', result: @survivor }, status: :ok
-		else
-			render json: { success: false, message: 'Survivor not found' }, status: :unprocessable_entity
-		end
+		render json: { success: true, message: 'Survivor found', result: @survivor }, status: :ok
 	end
 
 	# Create a new survivor
 	def create
 		@survivor = Survivor.new(survivor_params)
-
-		if @survivor.save
-			render json: { success: true, message: 'Survivor created', result: @survivor }, status: :created
-		else
-			render json: { success: false, message: @survivor.errors, result: @survivor }, status: :unprocessable_entity
-		end	
+		@survivor.save!
+		render json: { success: true, message: 'Survivor created', result: @survivor }, status: :created
 	end
 
 	# Delete a survivor
 	def destroy
 		@survivor.destroy
-		render json: { success: true, message: 'Survivor deleted', result: @survivor }, status: :ok
-
+		render json: { success: true, message: 'Survivor deleted', result: @survivor }, status: :accepted
 	end
 
 	# Update a survivor
 	def update
-		if @survivor.update_attributes(survivor_params)
-			render json: { success: true, message: 'Survivor updated', result: @survivor }, status: :accepted
-		else
-			render json: { success: false, message: @survivor.errors, result: @survivor }, status: :unprocessable_entity
-		end
-
+		@survivor.update!(survivor_params)
+		render json: { success: true, message: 'Survivor updated', result: @survivor }, status: :accepted
 	end
 
 	private
@@ -49,8 +38,15 @@ class Api::V1::SurvivorsController < ApplicationController
 		params.permit(:name, :age, :gender, :latitude, :longitude)
 	end
 
-	private
 	def set_survivor
 		@survivor = Survivor.find(params[:id])
+	end
+
+	def render_unprocessable_entity_response(exception)
+		render json: { success: false, errors: exception.record.errors }, status: :unprocessable_entity
+	end
+
+	def render_not_found_response(exception)
+		render json: { success: false, errors: exception.message }, status: :not_found
 	end
 end
