ae3cc0794b9a56cbdd13a6bf1289d4dc6a959c5f
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-23 12:22:21 -0300

    Report model refectored
    Now raise a RecordNotFound error when the abducted guy
    reported does not exist in Survivors

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,14 +1,16 @@
 class Report < ApplicationRecord
 	validates 	:reporter_id, :abducted_id, :message, presence: true
 	validates 	:reporter_id, :abducted_id, numericality: { only_integer: true }
-	after_save 	:update_flag_abducted_in_survivors
+	before_save :update_flag_abducted_in_survivors
 
 	private
 	# Flag the survivor as abducted if there is at least three reports for that survivor
 	def update_flag_abducted_in_survivors
+		# Raise a NotFount error if survivor not exist
+		@survivor = Survivor.find(abducted_id)
 		num_reports =  Report.where(abducted_id: abducted_id).count
 		if num_reports >= 3
-			Survivor.update(abducted_id, abducted: true)
+			@survivor.update(abducted: true)
 		end 
 	end
 end
