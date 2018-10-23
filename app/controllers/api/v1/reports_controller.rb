a0d20484162f063ac2d7c60af5a0194b22947a2a
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-23 12:17:04 -0300

    ReportController refectored

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -7,23 +7,20 @@ class Api::V1::ReportsController < ApplicationController
 		total_pages = (total_results.to_f/per_page).ceil
 
 		@reports = Report.paginate(:page => page, :per_page => per_page).order('created_at ASC')
-		render json: { 
+		json_response({ 
 				success: true,
 				page: page,
 				per_page: per_page,
 				total_pages: total_pages,
 				total_results: total_results,
 				results: @reports
-			}, status: :ok		
+			}, :ok)		
 	end	
 	# POST api/v1/reports
 	def create
 		@report = Report.new(report_params)
-		if @report.save
-			render json: { success: true, message: "Report created", report: @report }, status: :ok
-		else
-			render json: { success: false, message: "Failed to create report"}, status: :unprocessable_entity
-		end
+		@report.save!
+		json_response({ success: true, message: "Report created", report: @report }, :created)
 	end
 
 	private
