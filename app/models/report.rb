e7a398a62ba7ecb39a1021fff3e04b4dec992e06
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-23 10:50:34 -0300

    Report model refectored

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,10 +1,10 @@
 class Report < ApplicationRecord
-	validates :reporter_id, :abducted_id, presence: true
-	validates :reporter_id, :abducted_id, numericality: { only_integer: true }
-	after_save :update_flag_abducted_in_survivors
+	validates 	:reporter_id, :abducted_id, :message, presence: true
+	validates 	:reporter_id, :abducted_id, numericality: { only_integer: true }
+	after_save 	:update_flag_abducted_in_survivors
 
 	private
-	# Flag the survivor as abducted if there is at least three reports
+	# Flag the survivor as abducted if there is at least three reports for that survivor
 	def update_flag_abducted_in_survivors
 		num_reports =  Report.where(abducted_id: abducted_id).count
 		if num_reports >= 3
