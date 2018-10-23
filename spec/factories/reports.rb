e88d5917933a6698d064eab883a78b2100bb98fa
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-23 12:24:02 -0300

    Change report factory to ensure that the abducted_id exist in  Survivors
    But you still need to create at least 5 survivors

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
 	factory :report do
 		reporter_id { Faker::Number.between(1, 30) }
 		message { Faker::Lorem.sentence }
-		abducted_id { Faker::Number.between(1, 10) }
+		# Range index of abduced survivor need to be short
+		# To ensure that will be reported only survivors that exist
+		abducted_id { Faker::Number.between(1, 5) }
 	end
 end
