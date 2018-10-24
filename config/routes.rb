5355b9f389218a2ea842fb5137a0c5d8c94ae284
gedhean <gedhean.dev@gmail.com>
2018-10-23 21:06:37 -0300

    Added new route /survivors/stats

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
 	namespace 'v1' do
-	  		resources :survivors
-	  		resources :reports
+	  		get '/survivors/stats', to: 'survivors#stats', as: 'statistics'
+	  		resources :survivors, :reports
 	  	end  	
   end
 end
